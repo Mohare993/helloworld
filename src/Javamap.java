30a7d514be82e91cfd4815f4667f0b0ccec65469
Mohare993 <Matthew.O'Hare@academytrainee.com>
2019-07-16 11:22:45 +0100

    Javamap update

--- src/Javamap.java
+++ src/Javamap.java
@@ -1,30 +1,14 @@
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.HashMap;
+
+import java.util.Map;
 
 
 
 
 public class Javamap {
-;
-	private List<String> nums = new ArrayList<>();
 
-	public List<String> getNums() {
-		return nums;
-	}
-
-	public void setNums(List<String> nums) {
-		this.nums = nums;
-	}
 	
-	public boolean removeNum(String nums) {
-		return this.nums.remove(nums);
-
-	}
-	
-	public boolean addNum(String nums) {
-		return this.nums.add(nums);
-
-	}
-
+	Map<Integer, String> javaMap = new HashMap<Integer, String>();
 
 }
