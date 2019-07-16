c705fbadad497daf2eedd9fb7541b6128ed392e2
Mohare993 <Matthew.O'Hare@academytrainee.com>
2019-07-16 13:02:11 +0100

    Create Account Feature added

--- src/helloworld/Account.java
+++ src/helloworld/Account.java
@@ -1,48 +1,62 @@
 package helloworld;
 
 public class Account {
-	
-	private int id;
+
+	private int id = 0;
 	private int accountNumber;
 	private String firstName;
 	private String lastName;
-	
-			
-		public int getId() {
-		return id;
+	private static int counter;
+
+	public static int getNextId() {
+		return ++counter;
 	}
+	
+	public static int getId() {
+		return counter;
+	}
+
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public int getAccountNumber() {
 		return accountNumber;
 	}
+
 	public void setAccountNumber(int accountNumber) {
 		this.accountNumber = accountNumber;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	
-	public Account(int id, int accountNumber, String firstName, String lastName) {
+
+	public Account(int accountNumber, String firstName, String lastName) {
 		super();
-		this.id = id;
+		this.id = getNextId();
 		this.accountNumber = accountNumber;
 		this.firstName = firstName;
 		this.lastName = lastName;
 	}
-	
-	
-		
-		
+
+	@Override
+	public String toString() {
+		return "Account [id=" + id + ", accountNumber=" + accountNumber + ", firstName=" + firstName + ", lastName="
+				+ lastName + "]";
+	}
 
 }
--- src/helloworld/App.java
+++ src/helloworld/App.java
@@ -4,7 +4,21 @@ public class App {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world to the standard out");
+		
+		
+		Account test = new Account(2,"Bob","D");
+		System.out.print(test);
+		
+		JavaMap tastmap = new JavaMap();
+		
+		System.out.print(tastmap);
+		
+	
+		tastmap.createAccount(2334, "bill", "fgdgs");
 
+		System.out.print(tastmap);
+	
+		
 	}
 
 }
